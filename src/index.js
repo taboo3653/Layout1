d92e06d47fb052dfc676819a069092ae37c8e781
taboo3653 <agafonov3653@gmail.com>
2019-11-12 19:09:53 +0300

    push

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './styles/normalize.css'
+import './styles/styles.scss'
+
---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const path = require('path')
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin}  = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname,'/dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "./src"),
+        compress: true,
+        port: 3000,
+        watchContentBase: true,
+        progress: true
+      },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            },
+            { test: /\.handlebars$/, loader: "handlebars-loader" },
+            {
+                test: /\.(sc|sa|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                          publicPath: './',
+                          hmr: process.env.NODE_ENV === 'development',
+                        },
+                      },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                    
+                ]
+            },
+            {
+              test: /\.(svg|png|jpg|gif|ico)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+              use: [
+                    {
+                      loader: 'file-loader',
+                      options: {
+                        name: '[name].[ext]',
+                        outputPath: 'images/'
+                      }
+                    }
+                  ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[name].[ext]',
+                      outputPath: 'fonts/'
+                    }
+                  }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.LoaderOptionsPlugin({
+          options: {
+            handlebarsLoader: {}
+          }
+        }),
+        new MiniCssExtractPlugin({
+          filename: '[name].css',
+          chunkFilename: '[id].css',
+          ignoreOrder: false,
+        }),
+        new HtmlWebpackPlugin({
+          template: "./src/index.handlebars",
+          minify: {
+            collapseWhitespace: true
+            },
+        }),
+        
+      ]
+}
